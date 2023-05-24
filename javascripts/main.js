c486bcd720412e16e3bee640143f1e49d034e66c
Audrey Yang <audrey.butter0930@gmail.com>
2023-05-23 20:58:29 -0700

    basic formatting

---
+++ javascripts/main.js
@@ -0,0 +1,12 @@
+
+var faction = document.getElementById("faction");
+
+var factions = ["corpus", "grineer", "meridian", "arbiters", "suda", "perrin", "veil","loka"];
+var hexisPrefix = ["Strategist", "Probate", "Scholar", "Vigilant", "Centurion", "Venator", "Adept", "Arbiter", "Armsman", "Seneschal"];
+var sudaPrefix = ["Arch-Mem", "Disp-Arch", "Surv-Tel", "Expi-Dyna", "Log-Rec", "Seek-Loc", "Res-Arc", "Cura-Phano", "Pion-Rec", "Inves-Resp", "Ult-Pho"];
+
+/*-------------------------------------*/
+
+function generate() {
+    
+}
