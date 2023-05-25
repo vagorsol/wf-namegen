6c51cabadb7aef4b1222797d731ee7cad7196550
Audrey Yang <audrey.butter0930@gmail.com>
2023-05-24 23:01:55 -0700

    web styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 
 var faction = document.getElementById("faction");
 
-var factions = ["corpus", "grineer", "meridian", "arbiters", "suda", "perrin", "veil","loka"];
+var factions = ["corpus", "grineer", "hexis", "suda", "veil","loka"];
 var hexisPrefix = ["Strategist", "Probate", "Scholar", "Vigilant", "Centurion", "Venator", "Adept", "Arbiter", "Armsman", "Seneschal"];
 var sudaPrefix = ["Arch-Mem", "Disp-Arch", "Surv-Tel", "Expi-Dyna", "Log-Rec", "Seek-Loc", "Res-Arc", "Cura-Phano", "Pion-Rec", "Inves-Resp", "Ult-Pho"];
 
