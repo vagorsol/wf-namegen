3e663acb5c405bc9a2f8847f694b88d00ce74e29
Audrey Yang <audrey.butter0930@gmail.com>
2023-05-30 19:16:43 -0700

    pass name generation function by string

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,27 +6,65 @@ var sudaPrefix = ["Arch-Mem", "Disp-Arch", "Surv-Tel", "Expi-Dyna", "Log-Rec", "
 function generate() {
     var faction = document.getElementById("faction").value;
     var output = document.getElementById("result");
+    var names; // empty array for output
 
-    // temp text to prove that button works
-    // haven't figured out how to make global variables work but i'm getting the impression it just Doesn't™
-    output.textContent = faction; 
-
-    switch(faction) {
-        case "corpus":
-            break;
-        case "grineer":
-            break;
-        case "hexis":
-            break;
-        case "suda":
-            break;
-        case "veil":
-            break;
-        case "loka": 
-            break;
+    // alert if try to generate without selecting a faction, otherwise pass name of function to name generator function
+    if (!faction) {
+        alert("Please select a faction!");
+        return;
+    } else {
+        // pass function by name. i guess
+        names = genNames(faction);
     }
+
+    // convert to string to append
+    var nameslst = "";
+    for (let i = 0; i < names.length; i ++) {
+        // avoid prepending newline for first one for nice formating
+        if (i == 0) {
+            nameslst += names[i];
+        } else {
+            nameslst += ("\r\n" + names[i]);
+        }
+    }
+
+    // output names
+    output.textContent = nameslst; 
 }
 
-var grineer = new function() {
+// given a faction, use the associated name generation rules and return randomly generated names
+function genNames(faction) {
+    var names = [];
+    for (let i = 0; i < 10; i ++) {
+        var name = window[faction].apply(); 
+        names.push(name);
+    }
+    return names;
+}
+
+/*-------------------------------------*/
+// name generation rules
+
+function grineer() {
+    return "testing";
+}
+
+function corpus() {
+
+}
+
+function hexis() {
+
+}
+
+function suda() {
+
+}
+
+function veil() {
+
+}
+
+function loka() {
 
 }
